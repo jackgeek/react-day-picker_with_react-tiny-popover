104c99fd78f46a91549565926d47749fa578aaea
Jack Allan <jackgeek@gmail.com>
2021-03-11 15:59:28 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import DayPicker from "react-day-picker/DayPickerInput";
+import "react-day-picker/lib/style.css";
+
+export default function App() {
+  return (
+    <div className="spacer">
+      <DayPicker />
+    </div>
+  );
+}
